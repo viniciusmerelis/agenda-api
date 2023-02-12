eb92c65a633fc07108dcf05cb7cb3d8b20cc27d5
vinicius <vinicius.merelis@gmail.com>
2023-02-12 15:55:59 -0300

    Adicionando exception handle e problem detail

---
+++ src/main/java/com/agenda/core/validation/ValidacaoException.java
@@ -0,0 +1,12 @@
+package com.agenda.core.validation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+@AllArgsConstructor
+@Getter
+public class ValidacaoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private BindingResult bindingResult;
+}
