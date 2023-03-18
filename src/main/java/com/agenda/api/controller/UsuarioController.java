b6b3b47365d485618b0d6faa491bb82a9635d031
vinicius <vinicius.merelis@gmail.com>
2023-03-18 14:39:11 -0300

    Adicionando metodo de listagem e busca de usuarios

--- src/main/java/com/agenda/api/controller/UsuarioController.java
+++ src/main/java/com/agenda/api/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.agenda.api.controller;
 
 import com.agenda.api.mapper.UsuarioMapper;
+import com.agenda.api.model.UsuarioDTO;
 import com.agenda.domain.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -12,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
     private final UsuarioService service;
     private final UsuarioMapper mapper;
+
+    @GetMapping
+    public List<UsuarioDTO> listar() {
+        return mapper.toDto(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioDTO buscar(@PathVariable Long id) {
+        return mapper.toDto(service.buscar(id));
+    }
 }
